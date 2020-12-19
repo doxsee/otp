81a987193b4dd9e48968b40fba78a52cdc1ad4d3
Trevor Howard <tr3v10@gmail.com>
2020-12-18 21:39:25 -0700

    Added code comments and changed readline() function to read()

--- otp.py
+++ otp.py
@@ -16,6 +16,10 @@ Created on Sat Dec 12 20:32:41 2020
 import os
 import time
 
+
+## This main function is the CLI menu, utilizing both the otpe function for 
+## message encryption and the otpd function for message decryption.
+ 
 def main() :
     option = input("(E) to Encrypt - (D) to Decrypt - (Q) to Quit): ")
     if option.upper() == "E" :
@@ -44,6 +48,11 @@ def main() :
         main()
     return
 
+## This encryption function takes the message and creates two unique files. One
+## is the encrypted message stored as a list of UTF-8 characters in dec form,
+## while the other is the randomly created key. This key is the same lenght as
+## the message and is also stored as a list of UTF-8 characters in dec form.
+
 def otpe(message) :
     otp = []
     otpNum = []
@@ -65,7 +74,7 @@ def otpe(message) :
         cryptNum.append(messageNum[num] + ord(otp[num]))
         otpNum.append(ord(otp[num]))
         
-# Checks if there is another message already generated with the same filename.
+# Verify ciphertext and otp filenames are unique.
 
     if os.path.exists("ciphertext.txt") :
         outfile = open("ciphertext{}.txt".format(int(time.time())), "w")
@@ -82,30 +91,49 @@ def otpe(message) :
     otpfile.close()
     return cryptNum
 
+## This function takes the two files created by the otpe function and decrypts
+## them into a message.txt file. Creates a unique message file name if message
+## file already exists.
+
 def otpd(ciphertextInFile, otpInFile) :
+
+# Open the two files and store them in two corresponding variables.
+
     ciphertextFile = open(ciphertextInFile, "r")
     otpFile = open(otpInFile, "r")
-    
-    ciphertext = ciphertextFile.readline()
+
+# Reads the entire ciphertextFile and converts it into a list to be stored
+# in the ciphertextList variable.
+  
+    ciphertext = ciphertextFile.read()
     ciphertext = ciphertext.strip("[]")
     ciphertext = ciphertext.replace(" ", "")
     ciphertextList = []
     encryptedList = ciphertext.split(",")
     for element in encryptedList :
         ciphertextList.append(int(element))
-    
-    otp = otpFile.readline()
+        
+# Reads the entire otpFile and converts it into a list to be stored
+# in the otpList variable.
+
+    otp = otpFile.read()
     otp = otp.strip("[]")
     otp = otp.replace(" ", "")
     otpList = []
     encryptedList = otp.split(",")
     for element in encryptedList :
         otpList.append(int(element))
-    
+
+# Creates a list that stores the decrypted UTF-8 characters as numbers. The
+# loop is the programs decryption mechanism - taking the ciphertext and
+# subtracting the otp key.
+   
     decryptedMessage = []
     for num in range(len(ciphertextList)) :
         decryptedMessage.append(ciphertextList[num] - otpList[num])
-       
+        
+# Verify message filename is unique.
+
     if os.path.exists("message.txt") :
         message = open("message{}.txt".format(int(time.time())), "w")
         for element in decryptedMessage :
@@ -114,7 +142,9 @@ def otpd(ciphertextInFile, otpInFile) :
         message = open("message.txt", "w")
         for element in decryptedMessage :
             message.write(chr(element))
-    
+            
+# Close all open files
+
     message.close()
     ciphertextFile.close()
     otpFile.close()
